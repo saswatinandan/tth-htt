53db37d5a10814ed46980ddfe1b6df647af815db
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2019-09-05 15:53:40 +0300

    adding removing samples

--- python/samples/tthAnalyzeSamples_2017_BDT.py
+++ python/samples/tthAnalyzeSamples_2017_BDT.py
@@ -13,6 +13,26 @@ bdt_samples = [
   "TTToSemiLeptonic_PSweights",
   "TTToHadronic",
   "TTToHadronic_PSweights",
+  'signal_ggf_nonresonant_node_sm_hh_2b2t',
+  'signal_ggf_nonresonant_node_sm_hh_2b2v',
+  'signal_ggf_nonresonant_node_sm_hh_2v2t',
+  'signal_ggf_nonresonant_node_sm_hh_4t',
+  'signal_ggf_nonresonant_node_sm_hh_4v' ,
+  "GluGluHToTauTau",
+  "GluGluHToTauTau_ext1",
+  "GluGluHToZZTo4L",
+  "GluGluHToZZTo2L2Q_M125",
+  "GluGluHToWWToLNuQQ_M125_PSweights",
+  "GluGluHToWWTo2L2Nu_M125",
+  "VBFHToTauTau",
+  "VBF_HToZZTo4L",
+  "VBFHToWWToLNuQQ_M125_PSweights",
+  "VBFHToWWTo2L2Nu_M125",
+  "VHToNonbb_M125",
+  "ZH_HToBB_ZToLL",
+  "ZHToTauTau"
+  "TTWH",
+  "TTZH"
 ]
 
 for sample_name, sample_info in samples_2017.items():
--- python/samples/tthAnalyzeSamples_2018_BDT.py
+++ python/samples/tthAnalyzeSamples_2018_BDT.py
@@ -1,4 +1,4 @@
-from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2018 import samples_2018
+from tthAnalysis.HiggsToTauTau.samples.tthAnalyzeSamples_2018_base import samples_2018
 
 bdt_samples = [
   "ttHToNonbb_M125_powheg",
@@ -9,6 +9,20 @@ bdt_samples = [
   "TTTo2L2Nu",
   "TTToSemiLeptonic",
   "TTToHadronic",
+  "GluGluHToTauTau",
+  "GluGluHToZZTo4L",
+  "GluGluHToZZTo2L2Q_M125",
+  "GluGluHToWWToLNuQQ_M125",
+  "GluGluHToWWTo2L2Nu_M125",
+  "VBFHToTauTau_ext1",
+  "VBF_HToZZTo4L",
+  "VBFHToWWToLNuQQ_M125",
+  "VBFHToWWTo2L2Nu_M125",
+  "VHToNonbb_M125",
+  "ZH_HToBB_ZToLL",
+  "ZHToTauTau"
+  "TTWH_ext1",
+  "TTZH_ext1"
 ]
 
 for sample_name, sample_info in samples_2018.items():
