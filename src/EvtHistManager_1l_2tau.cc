db7ac950512c9c94e740ead26dd33d19555cc583
Alexandra Carvalho <alexandra.oliveira@cern.ch>
2019-09-17 11:55:55 +0300

    adding histo

--- src/EvtHistManager_1l_2tau.cc
+++ src/EvtHistManager_1l_2tau.cc
@@ -9,6 +9,7 @@ EvtHistManager_1l_2tau::EvtHistManager_1l_2tau(const edm::ParameterSet & cfg)
     "numElectrons",
     "numMuons",
     "numHadTaus",
+    "numHadTausMVA_medium",
     "numJets",
     "numBJets_loose",
     "numBJets_medium",
@@ -42,6 +43,7 @@ EvtHistManager_1l_2tau::bookHistograms(TFileDirectory & dir)
   histogram_numElectrons_    = book1D(dir, "numElectrons",    "numElectrons",     5, -0.5,  +4.5);
   histogram_numMuons_        = book1D(dir, "numMuons",        "numMuons",         5, -0.5,  +4.5);
   histogram_numHadTaus_      = book1D(dir, "numHadTaus",      "numHadTaus",       5, -0.5,  +4.5);
+  histogram_numHadTausMVA_medium_      = book1D(dir, "numHadTausMVA_medium",      "numHadTausMVA_medium",       5, -0.5,  +4.5);
   histogram_numJets_         = book1D(dir, "numJets",         "numJets",         20, -0.5, +19.5);
   histogram_numBJets_loose_  = book1D(dir, "numBJets_loose",  "numBJets_loose",  10, -0.5,  +9.5);
   histogram_numBJets_medium_ = book1D(dir, "numBJets_medium", "numBJets_medium", 10, -0.5,  +9.5);
@@ -57,6 +59,7 @@ void
 EvtHistManager_1l_2tau::fillHistograms(int numElectrons,
                                        int numMuons,
                                        int numHadTaus,
+                                       int numHadTausMVA_medium,
                                        int numJets,
                                        int numBJets_loose,
                                        int numBJets_medium,
@@ -69,6 +72,7 @@ EvtHistManager_1l_2tau::fillHistograms(int numElectrons,
   fillWithOverFlow(histogram_numElectrons_,    numElectrons,    evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_numMuons_,        numMuons,        evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_numHadTaus_,      numHadTaus,      evtWeight, evtWeightErr);
+  fillWithOverFlow(histogram_numHadTausMVA_medium_,      numHadTausMVA_medium,      evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_numJets_,         numJets,         evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_numBJets_loose_,  numBJets_loose,  evtWeight, evtWeightErr);
   fillWithOverFlow(histogram_numBJets_medium_, numBJets_medium, evtWeight, evtWeightErr);
